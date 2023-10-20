7b66eb3b0f98afb6f3d268aa2a8ec66e263e7f20
Saket Nanda <saketnanda164@gmail.com>
2023-10-20 10:45:29 +0530

    Added slider and added form validation

--- script.js
+++ script.js
@@ -19,3 +19,56 @@ hamburger.addEventListener('click', ()=>
     }
    
 });
+
+let slidesperview=3
+
+
+const mediaQuery2 = window.matchMedia('(max-width: 900px)')
+if (mediaQuery2.matches) {
+  
+    slidesperview = 2;
+  }
+
+const mediaQuery = window.matchMedia('(max-width: 466px)')
+if (mediaQuery.matches) {
+    console.log("mobile reached");
+    slidesperview = 1;
+  }
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: slidesperview,
+    spaceBetween: 2,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+//form validation
+
+  const form=document.getElementById("form");
+  const email=document.getElementById("email");
+  const sender_name=document.getElementById("sender_name");
+  const text=document.getElementById("text");
+  const submit_btn=document.getElementById("send_btn");
+
+
+  form.addEventListener("submit", (e)=>
+  {
+    e.preventDefault();
+    console.log(sender_name.value)
+    if(sender_name.value.length<3)
+    {
+        alert("Name must be at least 3 characters");
+    }
+    if(text.value.length<5)
+    {
+        alert("Message too short");
+    }
+  })
